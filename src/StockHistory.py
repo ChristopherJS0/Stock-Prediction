0c17a431ba627fa3b6b5d9abe46821ce30b4021d
ChristopherJS0 <chrisjsalinas@outlook.com>
2024-06-08 13:59:39 -0400

    Stock training complete and results of training and predictions are amazing.

--- src/StockHistory.py
+++ src/StockHistory.py
@@ -1,93 +1,113 @@
+import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
+from copy import deepcopy
 
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras import Adam
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras import layers
+TF_ENABLE_ONEDNN_OPTS = 0
 
-def makeValidDates(rawDate):
-    SplitDate = rawDate.split('-')
-    year,month,day = int(SplitDate[0]), int(SplitDate[1]), int(SplitDate[2])
-    return datetime.datetime(year=year, month=month, day=day)
+def createAiModel(xSet, ySet, xVals, yVals, epochs):
+    '''
+    param: set of X values to train on.
+    param: set of Y values to train on.
+    param: set of x validation values.
+    param: set of y validation values.
+    param: Number of times to train the model
 
-def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
-    firstDate = makeValidDates(first_date_str)
-    lastDate = makeValidDates(last_date_str)
+    rtype: Model created by tensorflow using the LSTM learning technique.
+    '''
+    
+    model = tf.keras.models.Sequential([tf.keras.layers.Input((xSet.shape,ySet.shape)),
+                        tf.keras.layers.LSTM(64),
+                        tf.keras.layers.Dense(32, activation='relu'),
+                        tf.keras.layers.Dense(32, activation='relu'),
+                        tf.keras.layers.Dense(1)])
+    
+    model.compile(loss='mse',
+              optimizer='Adam',
+              metrics=['mean_absolute_error'])
+ 
+    model.fit(x=xSet, y=ySet, validation_data=(xVals, yVals), epochs=epochs) 
 
-    target_date = firstDate
+    return model
 
-    dates = []
-    X, Y = [], []
-    last_time = False
+def predictFutureVals(dateVals, dateTests, xSet):
+    recursivePredictions = []
+    recursiveDates = np.concatenate([dateVals, dates_test])
 
-    while True:
-        # View until target date, the last n+1 elemns of
-        df_subset = dataframe.loc[:target_date].tail(n+1)
-        if len(df_subset) != n+1: # If subset doesn't have n+1 elems.
-            print(f'Error: Window of size {n} is too large for date {target_date}')
-            return
-
-        values = df_subset['Close'].to_numpy() # Make subset into array.
-        x, y = values[:-1], values[-1] # x = all vals until last, y = last val.
-
-        dates.append(target_date)
-        X.append(x)
-        Y.append(y)
-
-        next_week = dataframe.loc[target_date: target_date + datetime.timedelta(days=7)]
-        next_datetime_str = str(next_week.head(2).tail(1).index.values[0])
-        next_date_str = next_datetime_str.split('T')[0]
-
-        year_month_day = next_date_str.split('-')
-        year, month, day = year_month_day
-        next_date = datetime.datetime(day=int(day), month=int(month), year=int(year))
-
-        if last_time:
-            break
-
-    target_date = next_date
-
-    if target_date == lastDate:
-      last_time = True
-
-    ret_df = pd.DateFrame({})
-    ret_df['Target Date'] = dates
-
-    X = np.array(X)
-    for i in range(0, n):
-      X[:, i] # Until the ith element of the real X list into the array.
-      ret_df[f'Target-{n-i}'] = X[:, i]
-
-    ret_df['Target'] = Y
-    return ret_df
-
-def windowed_df_to_date_X_y(windowed_dataframe):
-    df_as_np = windowed_dataframe.to_numpy()
-
-    dates = df_as_np[:,0]
-    middle_matrix = df_as_np[:, 1: -1] # Get all rows, get the 2nd to the 2nd to last cols.
-    #   Tuple of amount of vecs (len(dates)), and 3 vecs, for 1 elem of vec
-    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
-    Y = df_as_np[:, -1]
-
-    return dates, X.astype(np.float32), Y.astype(np.float32)
+    for targetDate in recursiveDates:
+        lastWindow = deepcopy(X_train[-1])
+        next_pred = model.predict(np.array([lastWindow])).flatten()
+        recursivePredictions.append(next_pred)
+        lastWindow[-1] = next_pred
+    
+    return recursiveDates, recursivePredictions
 
 if __name__ == '__main__':
-# Make sure there's a csv file filled with the company's stock history.
-    StockDF = pd.read_csv('Stock.csv')
-    StockDF['Date'] = StockDF['Date'].apply(makeValidDates)
-    StockDF.index = StockDF.pop('Date')
-    plt.plot(StockDF.index, StockDF['Close'])
+    # Make sure there's a csv file filled with the company's stock history.
+    StockDF = pd.read_csv('AppleStock.csv')
+    
+    '''plt.plot(StockDF['Date'], StockDF['Close'])
+    plt.title('Dates and Close Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Close Value')
+    '''
 
-    #Take a year of stock data up to now.
-    today = datetime.now().date()
-    today = str(today)
-    lastYear = list(today)
-    lastYear[3] = str(int(lastYear[3]) - 1)
-    lastYear = ''.join(lastYear)
+    # Preparing data to use for the training.
+    xVals = StockDF['Date'].to_numpy()
+    yVals = StockDF['Close'].to_numpy()
 
-    yearDF = df_to_windowed_df(StockDF, lastYear, today, n=3)
-    allDates,bigX, y = windowed_df_to_date_X_y(yearDF)
+    print(xVals.shape)
+    
+    q_80 = int(len(yVals) * .8) # 80% of allDates
+    q_90 = int(len(yVals) * .9) # 90% of allDates
+
+    # Training sets. First 80% of data used to train.
+    dates_train, X_train, y_train = xVals[:q_80], xVals[:q_80], yVals[:q_80]
+    # Validation data. Next 10% of data used to validate the training.
+    dates_val, X_val, y_val = xVals[q_80:q_90], xVals[q_80:q_90], yVals[q_80:q_90]
+    # Testing data.
+    dates_test, X_test, y_test = xVals[q_90:], xVals[q_90:], yVals[q_90:]
+
+    # Get the AI model to train, val, and test.
+    model = createAiModel(X_train, y_train, X_val, y_val, 100)
+    
+    # Plotting the training graph.
+    trainPreds = model.predict(X_train).flatten()
+    plt.plot(dates_train, trainPreds)
+    plt.plot(dates_train, y_train)
+    plt.legend(['Training Predictions', 'Training Observations'])
+
+    # Plotting the validation prediction graph.
+    val_predictions = model.predict(X_val).flatten()
+    plt.plot(dates_val, val_predictions)
+    plt.plot(dates_val, y_val)
+    plt.legend(['Validation Predictions', 'Validation Observations'])
+
+    # Plotting the testing prediction graph.
+    test_predictions = model.predict(X_test).flatten()
+    plt.plot(dates_test, test_predictions)
+    plt.plot(dates_test, y_test)
+    plt.legend(['Testing Predictions', 'Testing Observations'])
+
+    #rDates, rPreds = predictFutureVals(dates_val, dates_test, X_train)
+
+    plt.plot(dates_train, trainPreds)
+    plt.plot(dates_train, y_train)
+    plt.plot(dates_val, val_predictions)
+    plt.plot(dates_val, y_val)
+    plt.plot(dates_test, test_predictions)
+    plt.plot(dates_test, y_test)
+    #plt.plot(rDates, rPreds)
+    plt.legend(['Training Predictions', 
+                'Training Observations',
+                'Validation Predictions', 
+                'Validation Observations',
+                'Testing Predictions', 
+                'Testing Observations'])
 
